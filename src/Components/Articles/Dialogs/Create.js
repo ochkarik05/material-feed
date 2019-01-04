97e0a783c06aa73ea56fe1246694d070110e5e31
vladimir <vladimir.kondratenko@idapgroup.com>
2019-01-04 17:35:54 +0200

    Add text fields to dialog

--- src/Components/Articles/Dialogs/Create.js
+++ src/Components/Articles/Dialogs/Create.js
@@ -1,63 +1,119 @@
 import React, {Component} from 'react';
 import {
-  Dialog,DialogTitle,DialogContent,DialogContentText,TextField,DialogActions,Button
+    Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Button,
 } from '@material-ui/core';
 
-import AddArticle from './../../AddArticle'
+import AddArticle from './../../AddArticle';
 
 export default class extends Component {
 
-  state = {
-    open: false,
-  };
+    state = {
+        open: false,
+        article: {
+            title: '',
+            image: '',
+            description: '',
+            content: '',
+            category: '',
+        },
+    };
 
-  handleToggle = () => {
+    handleChange = (name) => ({target: {value}}) => {
 
-    this.setState(prev => ({
-      open: !prev.open,
-    }));
+        this.setState((prevState) => ({
+            article: {
+                ...prevState.article,
+                [name]: value,
+            },
+        }));
 
-  };
+    };
 
-  render() {
+    handleToggle = () => {
 
-    const { open } = this.state;
+        this.setState(prev => ({
+            open: !prev.open,
+        }));
 
-    return <>
+    };
 
-      <AddArticle onClick={this.handleToggle}/>
+    render() {
 
-      <Dialog
-          open={open}
-          onClose={this.handleToggle}
-          aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send
-            updates occasionally.
-          </DialogContentText>
-          <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Email Address"
-              type="email"
-              fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={this.handleToggle} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={this.handleToggle} color="primary">
-            Subscribe
-          </Button>
-        </DialogActions>
-      </Dialog>
+        const {
+            open, article: {
+                title,
+                image,
+                description,
+                content,
+                category,
+            },
+        } = this.state;
 
-    </>;
-  }
+        return <>
+
+            <AddArticle onClick={this.handleToggle}/>
+
+            <Dialog
+                open={open}
+                onClose={this.handleToggle}
+                aria-labelledby="form-dialog-title"
+            >
+                <DialogTitle id="form-dialog-title">Add article</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        To add article, fill out fields below.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Title"
+                        type="text"
+                        fullWidth
+                        value={title}
+                        onChange={this.handleChange('title')}
+                    />
+
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Image"
+                        type="text"
+                        fullWidth
+                        value={image}
+                        onChange={this.handleChange('image')}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Description"
+                        type="text"
+                        fullWidth
+                        value={description}
+                        onChange={this.handleChange('description')}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        label="Content"
+                        type="text"
+                        multiline
+                        rows={20}
+                        fullWidth
+                        value={content}
+                        onChange={this.handleChange('content')}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.handleToggle} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={this.handleToggle} color="primary">
+                        Create
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+        </>;
+    }
 
 }
